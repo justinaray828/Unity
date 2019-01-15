08d22a49d9a620d7766d2436a59d973309b95d8c
justinaray828 <justinaray828@gmail.com>
2019-01-14 22:29:19 -0500

    Updated MenuUI script and added a LevelManager. Inputhandler is likely not needed at the moment.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    private int currentSceneIndex;
+    [SerializeField] private int MainMenuSceneIndex = 0;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(MainMenuSceneIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -9,53 +9,19 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class MenuUI : MonoBehaviour
 { 
-    private string cancelInput = "Cancel"; //Unity input manager axis
-
     [Header("Add Child Menu to toggle")]
     [SerializeField] private GameObject menu;
-    [Header("Main menu scene index if one exist in game")]
-    [SerializeField] private int MainMenuSceneIndex = 0;
 
     private bool menuToggleBool = false;
-    private bool cancelKeyPressed = false;
+    private string cancelInputString = "Cancel"; //Unity input manager axis
 
     void Update()
     {
-        if( CancelKeyIsPressed() )
+        if( Input.GetButtonDown(cancelInputString) )
         {
             menu.SetActive(menuToggleBool);
             menuToggleBool = !menuToggleBool;
         }
     }
 
-    private bool CancelKeyIsPressed()
-    {
-        if( Input.GetAxis(cancelInput) > 0 && !cancelKeyPressed)
-        {
-            cancelKeyPressed = true;
-            return true;
-        }
-
-        if (Input.GetAxis(cancelInput) == 0)
-        {
-            cancelKeyPressed = false;
-        }
-
-        return false;
-    }
-
-    public void CloseGame()
-    {
-        Application.Quit();
-    }
-
-    public void GoToScene(int sceneNumber)
-    {
-        SceneManager.LoadScene(sceneNumber);
-    }
-
-    public void GoToMainMenu()
-    {
-        SceneManager.LoadScene(MainMenuSceneIndex);
-    }
 }
